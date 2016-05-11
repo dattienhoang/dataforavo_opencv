e340d4606a506485b2f9539fa4fb6278d2443038
dattienhoang <dth2113@columbia.edu>
2016-05-11 11:02:50 -0400

    Add files via upload

---
+++ facerec_frdict.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 27 12:41:27 2016
+
+Reference:
+http://hanzratech.in/2015/02/03/face-recognition-using-opencv.html
+
+@author: Dat Tien Hoang
+"""
+
